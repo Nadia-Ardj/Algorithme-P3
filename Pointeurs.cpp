cf575b2ea6292bd5a03b2a8a171646e94b5c91ec
Nadia-Ardj <nadia.ardjoune.08@gmail.com>
2026-03-08 22:25:22 +0100

    Update Pointeurs.cpp

--- Pointeurs.cpp
+++ Pointeurs.cpp
@@ -2,7 +2,7 @@
 #include<cmath>
 
 using namespace std ;
-/*
+
 // 1- Comprendre la mémoire :
  quand tu fais int a=5 :
  L’ordinateur :
@@ -45,220 +45,220 @@ int main(){
 
     cout<<"a = "<<a<<endl;
     cout<<"*p = "<<*p<<endl;
-    */
+    
 //     Modifier une variable avec un pointeur :
-    // int main(){
-    //     int *p ;
-    //     int a=10 ;
-    //    p = &a ;
-    //     *p = 20;
-    //     cout<<a<<endl;
-    //     return 0;
-    //
-    // }
+    int main(){
+        int *p ;
+        int a=10 ;
+       p = &a ;
+        *p = 20;
+        cout<<a<<endl;
+        return 0;
+    
+    }
 //   Exercice simple :
-// Écris un programme qui :
-//
-// déclare une variable int x = 7
-// déclare un pointeur p
-// fait pointer p vers x
-// affiche :
-//
-// valeur de x
-// adresse de x
-// valeur de p
-// valeur pointée par p
+Écris un programme qui :
 
-// int main(){
-//     int x=7 ;
-//     int *p ;
-//     p = &x;
-//
-//     cout<<"valeur de x : "<<x<<endl;
-//     cout<<"adresse de x : "<<&x<<endl;
-//     cout<<"valeur de p :"<<p<<endl;
-//     cout<<"valeur pointé par p "<<*p<<endl;
-//     return 0;
-//
-// }
+déclare une variable int x = 7
+déclare un pointeur p
+fait pointer p vers x
+affiche :
+
+valeur de x
+adresse de x
+valeur de p
+valeur pointée par p
+
+int main(){
+    int x=7 ;
+    int *p ;
+    p = &x;
+
+    cout<<"valeur de x : "<<x<<endl;
+    cout<<"adresse de x : "<<&x<<endl;
+    cout<<"valeur de p :"<<p<<endl;
+    cout<<"valeur pointé par p "<<*p<<endl;
+    return 0;
+
+}
 // Exercice 2 :
-// créer int a = 5
-// créer int *p = &a
-// changer la valeur de a avec le pointeur
-// afficher a
+créer int a = 5
+créer int *p = &a
+changer la valeur de a avec le pointeur
+afficher a
 
-// int main(){
-//     int a=5;
-//     int *p = &a;
-//     *p = 22;
-//     cout<<"a = "<<a<<endl;
-//     return 0;
-//
-// }
+int main(){
+    int a=5;
+    int *p = &a;
+    *p = 22;
+    cout<<"a = "<<a<<endl;
+    return 0;
+
+}
 // Pointeur dans les fonction :
-// void change(int *x){
-//     *x=100;
-// }
-// int main(){
-//     int a=2;
-//     change(&a);
-//     cout<<"a= "<<a<<endl;
-//     return 0;
-// }
+void change(int *x){
+    *x=100;
+}
+int main(){
+    int a=2;
+    change(&a);
+    cout<<"a= "<<a<<endl;
+    return 0;
+}
 // Exercice classique pour échanger deux variables :
-// void change( int *a , int *b){
-//     int temp = *a;
-//     *a = *b ;
-//     *b = temp ;
-// }
-// int main(){
-//     int x=2;
-//     int y=3;
-//     change(&x,&y);
-//     cout<<"x= "<<x<<endl<<"y= "<<y<<endl;
-// }
+void change( int *a , int *b){
+    int temp = *a;
+    *a = *b ;
+    *b = temp ;
+}
+int main(){
+    int x=2;
+    int y=3;
+    change(&x,&y);
+    cout<<"x= "<<x<<endl<<"y= "<<y<<endl;
+}
 // // Exercice :
-// Écrire une fonction :
-//
-// void doubleValeur(int *x)
-// qui multiplie une variable par 2.
+Écrire une fonction :
 
-// void doubleValeur(int *x){
-//     *x *=2;
-// }
-// int main(){
-//     int a=2;
-//     doubleValeur(&a);
-//     cout<<"a = "<<a<<endl;
-//   return 0;
-// }
+void doubleValeur(int *x)
+qui multiplie une variable par 2.
+
+void doubleValeur(int *x){
+    *x *=2;
+}
+int main(){
+    int a=2;
+    doubleValeur(&a);
+    cout<<"a = "<<a<<endl;
+  return 0;
+}
 // Exercice :
-// Écrire une fonction :
-//
-// void addition(int *a, int *b)
-// qui ajoute b à a.
+Écrire une fonction :
 
-// void addition(int *a , int *b , int *c){
-//      *c = *a + *b ;
-// }
-// int main(){
-//     int x=2;
-//     int y=3;
-//     int c=0;
-//     addition(&x,&y,&c);
-//     cout<<" a+b = "<<c<<endl;
-//
-//     return 0;
-//
-// }
-//
-// // L'allocation dynamique :
-// int a=5 ;
-// ci, la mémoire est créée automatiquement par le programme.
-// Mais parfois on veut créer de la mémoire pendant l’exécution.
-// Pour cela on utilise : new
-//
-// int main(){
-//
-//     int *p;
-//     p = new int;
-//     *p = 10;
-//     cout<<*p<<endl;
-//     delete p ;
-//     return 0;
-// }
-//
-// Quand on utilise new, il faut libérer la mémoire
-// On utilise : delete
-//
-// new --> delete ; sinon on vas créer une fuite mémoire(memory leak)
-//
-// // Exercice 1 :
-// Écrire un programme qui :
-//
-// crée un entier dynamique
-// met la valeur 50
-// affiche la valeur
-// libère la mémoire
+void addition(int *a, int *b)
+qui ajoute b à a.
 
-// int main(){
-//     int *p = new int ;
-//     *p = 50 ;
-//     cout<<*p<<endl;
-//     delete p;
-//     return 0;
-//
-// }
+void addition(int *a , int *b , int *c){
+     *c = *a + *b ;
+}
+int main(){
+    int x=2;
+    int y=3;
+    int c=0;
+    addition(&x,&y,&c);
+    cout<<" a+b = "<<c<<endl;
+
+    return 0;
+
+}
+
+// L'allocation dynamique :
+int a=5 ;
+ci, la mémoire est créée automatiquement par le programme.
+Mais parfois on veut créer de la mémoire pendant l’exécution.
+Pour cela on utilise : new
+
+int main(){
+
+    int *p;
+    p = new int;
+    *p = 10;
+    cout<<*p<<endl;
+    delete p ;
+    return 0;
+}
+
+Quand on utilise new, il faut libérer la mémoire
+On utilise : delete
+
+new --> delete ; sinon on vas créer une fuite mémoire(memory leak)
+
+// Exercice 1 :
+Écrire un programme qui :
+
+crée un entier dynamique
+met la valeur 50
+affiche la valeur
+libère la mémoire
+
+int main(){
+    int *p = new int ;
+    *p = 50 ;
+    cout<<*p<<endl;
+    delete p;
+    return 0;
+
+}
 // Exercice 2 :
-// Créer un tableau dynamique de 4 entiers
+Créer un tableau dynamique de 4 entiers
 
-// int main(){
-//     int *tab = new int[4];
-//     tab[0] = 2;
-//     tab[1] = 4;
-//     tab[2] = 6;
-//     tab[3] = 8;
-//
-//     for(int i=0;i<4;i++){
-//         cout<<tab[i]<<endl;
-//     }
-//     delete [] tab;
-//     return 0;
-// }
-//
+int main(){
+    int *tab = new int[4];
+    tab[0] = 2;
+    tab[1] = 4;
+    tab[2] = 6;
+    tab[3] = 8;
+
+    for(int i=0;i<4;i++){
+        cout<<tab[i]<<endl;
+    }
+    delete [] tab;
+    return 0;
+}
+
 // // Tableau et pointeur :
-// int *p = tab;
-// *p → valeur de tab[0]
-// *(p+1) → valeur de tab[1]
-// On peut créer un pointeur vers un tableau
-// int tab[3] = {10,20,30};
-// int *p = tab;
-// tab[0] = *p;
-// tab[1] = *p+1;
-// tab[2] = *p+2;
-// int main(){
-//
-//     int tab[3] = {10,20,30};
-//     int *p = tab;
-//
-//     cout<<*p<<endl;
-//     cout<<*(p+1)<<endl;
-//     cout<<*(p+2)<<endl;
-//     return 0;
-// }
-//
-// // Parcourir un tableau avec un pointeur :
-// int main(){
-//
-//     int tab[4] = {2,4,6,8};
-//     int *p = tab;
-//     for(int i=0;i<4;i++){
-//       cout<<*(p+i)<<endl;
-//     }
-//
-// }
-//
-// // Passage d’un tableau à une fonction :
-// void afficher(int *tab,int n){
-//
-//     for(int i=0;i<n;i++)
-//         cout<<tab[i]<<endl;
-//
-// }
-// int main(){
-//     int tab[3] = {1,2,3};
-//     afficher(tab,3);
-// }
-// // exemple:
-// int tab[3] = {2,4,6};
-// int *p = tab;
-// p = p + 2;
-// cout<<*p<<endl;
-// // explication
-// resultat => 6
-// p = tab → pointe sur tab[0]
-// p = p + 2 → pointe sur tab[2]
-// *p → valeur de tab[2]
+int *p = tab;
+*p → valeur de tab[0]
+*(p+1) → valeur de tab[1]
+On peut créer un pointeur vers un tableau
+int tab[3] = {10,20,30};
+int *p = tab;
+tab[0] = *p;
+tab[1] = *p+1;
+tab[2] = *p+2;
+int main(){
+
+    int tab[3] = {10,20,30};
+    int *p = tab;
+
+    cout<<*p<<endl;
+    cout<<*(p+1)<<endl;
+    cout<<*(p+2)<<endl;
+    return 0;
+}
+
+ // Parcourir un tableau avec un pointeur :
+int main(){
+
+    int tab[4] = {2,4,6,8};
+    int *p = tab;
+    for(int i=0;i<4;i++){
+      cout<<*(p+i)<<endl;
+    }
+
+}
+
+// Passage d’un tableau à une fonction :
+void afficher(int *tab,int n){
+
+    for(int i=0;i<n;i++)
+        cout<<tab[i]<<endl;
+
+}
+int main(){
+    int tab[3] = {1,2,3};
+    afficher(tab,3);
+}
+// exemple:
+int tab[3] = {2,4,6};
+int *p = tab;
+p = p + 2;
+cout<<*p<<endl;
+// explication
+resultat => 6
+p = tab → pointe sur tab[0]
+p = p + 2 → pointe sur tab[2]
+*p → valeur de tab[2]
 
 // Tableau dynamique + fonction :
 void saisir( int *tab n , int n){
